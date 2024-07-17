48c0134e6e52bb858620e1f0b27fffb997d8823f
raghveerdeol <deolraghveer5@gmail.com>
2024-07-17 17:27:25 +0200

    create card

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,21 @@
 @section('title','Homepage')
 
 @section('main-content')
-    <h1>Welcome to homepage</h1>
-    @dd($movies)
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-4 mb-2">
+                    <div class="card border border-primary" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Original title: {{$movie->original_title}}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Title: {{$movie->title}}</h6>
+                            <p class="card-text">Nazionality: {{$movie->nationality}}</p>
+                            <p class="card-text">Released: {{$movie->date}}</p>
+                            <p class="card-text">Vote: {{$movie->vote}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,1 @@
-<h1>Header</h1>
+<h1 class="text-center m-4">Movies</h1>
